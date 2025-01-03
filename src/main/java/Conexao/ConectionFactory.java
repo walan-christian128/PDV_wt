bb7a1ada0cff970638543722f2fef6f14d942cd6
walan-christian128 <walancristiano@yahoo.com.br>
2025-01-03 14:14:56 -0300

    edit,war

--- src/main/java/Conexao/ConectionFactory.java
+++ src/main/java/Conexao/ConectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConectionFactory {
-    private static final String URL_TEMPLATE = "jdbc:mysql://localhost:3307/";
+    private static final String URL_TEMPLATE = "jdbc:mysql://localhost:3307/%s?useSSL=false";
     private static final String USER = "walan";  // Ajuste o usuário do banco de dados
     private static final String PASSWORD = "359483wa@";  // Ajuste a senha do banco de dados
 
