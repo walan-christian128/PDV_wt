8d0f941ed3c3c2c650145b69c7bb4c6ddd4711fd
walan-christian128 <walancristiano@yahoo.com.br>
2025-01-03 15:35:37 -0300

    editwar

--- src/main/java/Conexao/ConectionFactory.java
+++ src/main/java/Conexao/ConectionFactory.java
@@ -1,20 +1,22 @@
 package Conexao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConectionFactory {
-    private static final String URL_TEMPLATE = "jdbc:mysql://localhost:3307/%s?useSSL=false";
-    private static final String USER = "walan";  // Ajuste o usuário do banco de dados
-    private static final String PASSWORD = "359483wa@";  // Ajuste a senha do banco de dados
-
-    public Connection getConnection(String databaseName) throws ClassNotFoundException {
+    public Connection getConnection(String databaseName) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = String.format(URL_TEMPLATE, databaseName);
-            return DriverManager.getConnection(url, USER, PASSWORD);
-        } catch (SQLException e) {
+            Context ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/MyDB");
+            // Retorne a conexão do DataSource
+            Connection connection = ds.getConnection();
+            connection.setCatalog(databaseName); // Define o banco de dados dinamicamente
+            return connection;
+        } catch (NamingException | SQLException e) {
             throw new RuntimeException("Erro ao conectar ao banco de dados: " + databaseName, e);
         }
     }
